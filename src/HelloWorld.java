221ffa09152828a5622dcec451e95a406ee14006
shiyanchen <shiyc@cpchain.io>
2019-11-25 19:29:22 +0800

    try some class component functions and variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,29 @@
 public class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("Hello world!");
+    String x;
+    public HelloWorld(String input)
+    {
+        System.out.println("new object created with input = "+input);
+        x = input;
+    }
+    public void setX(String input)
+    {
+        x = input;
+    }
+    public String getX()
+    {
+        return x;
+    }
+    public static void main(String[] args)
+    {
+        HelloWorld myWorld = new HelloWorld("haha");
+        if(args.length>0)
+        {
+            myWorld.setX(args[0]);
+            System.out.println(myWorld.getX());
+        }
+        else
+        {
+            System.out.println("an argument is required!");
+        }
     }
 }
