e63117992202a39974329bd586f6118ce4cf3b48
shiyanchen <shiyc@cpchain.io>
2019-11-26 19:56:37 +0800

    contruct a foo function, very simple one!

--- src/foo.java
+++ src/foo.java
@@ -1,3 +1,17 @@
 public class foo {
-
+    string name;
+    public foo (String input) {
+        if (input == "") {
+            name = "nil"
+        }
+        else {
+            name = input;
+        }
+    }
+    public void setName (String name) {
+        this.name = name;
+    }
+    public String getName () {
+        return name
+    }
 }
