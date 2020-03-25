b409ff2ac965e1696ffe33e91983e10bd0380179
施 炎辰 <shiyc@cpchain.io>
2020-03-25 16:42:42 +0800

    add comments

--- Java/Thinking In Java/HelloDate.java
+++ Java/Thinking In Java/HelloDate.java
@@ -4,8 +4,10 @@ import java.util.*;
 
 public class HelloDate 
 {
+	// do not forget to capitalize S in string 
 	public static void main(String[] args)
 	{
+		// unlike golang, println does not capitalized 
 		System.out.println("Hello, it is  ");		
 		System.out.println(new Date());
 	}
