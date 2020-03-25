ce16797bd74792ca0288a4bb99625da0d6cd56ac
施 炎辰 <shiyc@cpchain.io>
2020-03-25 16:03:18 +0800

    first java code from thinking in java

---
+++ Java/Thinking In Java/HelloDate.java
@@ -0,0 +1,12 @@
+// HelloDate.java
+
+import java.util.*;
+
+public class HelloDate 
+{
+	public static void main(string[] args)
+	{
+		System.out.Println("Hello, it is  ");
+		System.out.Println(new Date());
+	}
+}
