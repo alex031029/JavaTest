8c683674dd55b89de9e4aea2942ab2d0db0e01e9
施 炎辰 <shiyc@cpchain.io>
2020-04-04 19:39:18 +0800

    add no new line print and printf

--- Java/Thinking In Java/HelloDate.java
+++ Java/Thinking In Java/HelloDate.java
@@ -7,8 +7,11 @@ public class HelloDate
 	// do not forget to capitalize S in string 
 	public static void main(String[] args)
 	{
+		int n = 10;
 		// unlike golang, println does not capitalized 
 		System.out.println("Hello, it is  ");		
 		System.out.println(new Date());
+		System.out.print("no new line print");
+		System.out.printf("format print %d\n", n);
 	}
 }
