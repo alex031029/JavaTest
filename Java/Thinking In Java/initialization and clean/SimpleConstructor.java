bc4c7a1244bd087306d2ced04a26eaf38c41bfaa
施 炎辰 <shiyc@cpchain.io>
2020-04-04 20:16:27 +0800

    a simple contructor demonstration

---
+++ Java/Thinking In Java/initialization and clean/SimpleConstructor.java
@@ -0,0 +1,21 @@
+import java.util.*;
+// a demonstration for simple constructor
+
+class Rock
+{
+	Rock()
+	{
+		System.out.println("Rock: ");
+	}
+}
+
+public class SimpleConstructor
+{
+	public static void main(String []args)
+	{
+		for(int i=0;i<10;i++)
+		{
+			new Rock();
+		}
+	}
+}
