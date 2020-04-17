e6bb3027bbb86f0c0f2216c141a6af201fcb06b2
Yanchen Shi <yansh@microsoft.com>
2020-04-17 15:47:29 +0800

    add string format comments

--- C#/HelloWorld/HelloWorld/Program.cs
+++ C#/HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,10 @@ namespace HelloWorldApplication
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World");
+			// {0} is a kind of string format, where 0 indicates the index of object. `sizeof(int)` is {0} in the line below
+			// while "haha" is the {1} 
+			Console.WriteLine("Hello World {0}, {0:C2}, {1}", sizeof(int), "haha");
+			// read a key from standard input
 			Console.ReadKey();
 		}
 	}
