a78dc628e07be54f71c61d616860ec5f5a22b4ed
Yanchen SHI <yansh@microsoft.com>
2020-04-17 15:58:01 +0800

    Try String and @

--- C#/HelloWorld/HelloWorld/Program.cs
+++ C#/HelloWorld/HelloWorld/Program.cs
@@ -7,8 +7,10 @@ namespace HelloWorldApplication
 		static void Main(string[] args)
 		{
 			// {0} is a kind of string format, where 0 indicates the index of object. `sizeof(int)` is {0} in the line below
-			// while "haha" is the {1} 
-			Console.WriteLine("Hello World {0}, {0:C2}, {1}", sizeof(int), "haha");
+			// while String s is the {1} 
+			// @ is to omit escape operator \
+			String s = @"C:\Windows";
+			Console.WriteLine("Hello World {0}, {0:C2}, {1}", sizeof(int), s);
 			// read a key from standard input
 			Console.ReadKey();
 		}
