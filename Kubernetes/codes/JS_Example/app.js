dd638cef9f40e89da6e557a3c73d22edefac4190
Yanchen Shi <yansh@microsoft.com>
2020-10-25 14:45:47 +0800

    minor modification

--- Kubernetes/codes/JS_Example/app.js
+++ Kubernetes/codes/JS_Example/app.js
@@ -4,6 +4,7 @@ const os = require('os');
 console.log("Yanchen Kubia server starting...");
 
 var handler = function(request, response) {
+	console.log("Let's start");
 	console.log("Received request from " + request.connection.remoteAddress);
 	response.writeHead(200);
 	response.end("You've hit " + os.hosename() + "\n");
