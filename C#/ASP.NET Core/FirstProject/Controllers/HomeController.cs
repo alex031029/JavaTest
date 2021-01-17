3125e45da2f86ca8908fab2a8a73c98b5f327229
Yanchen Shi <yansh@microsoft.com>
2021-01-18 00:06:02 +0800

    new action for testing

--- C#/ASP.NET Core/FirstProject/Controllers/HomeController.cs
+++ C#/ASP.NET Core/FirstProject/Controllers/HomeController.cs
@@ -11,27 +11,39 @@ namespace FirstProject.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        //private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+
+        // if I comment all methods, and leave Index() as the only method in Controller
+        // We can see a "Hello World" string in the browser. 
+        // public string Index()
+        // {
+        //    return "Hello World!";
+        // }
+
+        public ViewResult Index()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View("MyView");
         }
     }
 }
