4019233320afee6d71197f585529dbf42ab8a53a
Yanchen Shi <yansh@microsoft.com>
2021-01-25 17:21:32 +0800

    enrich comments

--- C#/note/lambda/lambda/Program.cs
+++ C#/note/lambda/lambda/Program.cs
@@ -18,6 +18,15 @@ namespace lambda
             };
             Console.WriteLine("offset is {0}", offset);  // offset is still 2
             Console.WriteLine(square2(5));      // 49
+
+            // Action, indicating no return value
+            Action<string> print = name => Console.WriteLine(name);
+            print("yansh");
+
+            // more than parameters
+            // we can also explictly write down the input type
+            Func<int, int, int> add = (int x, int y) => x + y;
+            Console.WriteLine(add(1, 2));
         }
     }
 }
