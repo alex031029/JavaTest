1448cf49b21beb81aecefa70883eee5ca5b20b25
Yanchen SHI <yansh@microsoft.com>
2021-01-29 17:39:03 +0800

    Create the sln for seperating emails

---
+++ C#/Yes, Your Majesty!/SeperateMailAddress/SeperateMailAddress/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeperateMailAddress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
