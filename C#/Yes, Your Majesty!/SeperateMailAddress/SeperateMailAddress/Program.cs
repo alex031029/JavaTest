e8e929a0ae6a6f5f2ac6e897c297cd5ed5f8991d
Yanchen SHI <yansh@microsoft.com>
2021-01-29 17:50:23 +0800

    enrich comment

--- C#/Yes, Your Majesty!/SeperateMailAddress/SeperateMailAddress/Program.cs
+++ C#/Yes, Your Majesty!/SeperateMailAddress/SeperateMailAddress/Program.cs
@@ -7,6 +7,8 @@ namespace SeperateMailAddress
         static void Main(string[] args)
         {
             // the exe file is in bin/Debug/netcoreapp3.1/ folder
+            // NB: I should write a try here
+            // both ../../../ and ./ should be tried
             var text = System.IO.File.ReadAllLines(@"../../../test.input");
             System.Console.WriteLine(text[0]);
         }
