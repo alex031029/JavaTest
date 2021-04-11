4cafeac94db411613ca1c208ee61d61cf65a0b7e
Yanchen Shi <yansh@microsoft.com>
2021-04-11 16:23:20 +0800

    add repository

---
+++ C#/ASP.NET Core/PartyInvites/Models/Repository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PartyInvites.Models
+{
+    public static class Repository
+    {
+        private static List<GuestResponse> responses = new List<GuestResponse>();
+        // Reponses is an expression-bodied member
+        // Reponses is a readonly property, and it returns the private value responses
+        public static IEnumerable<GuestResponse> Responses => responses;
+        public static void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+    }
+}
